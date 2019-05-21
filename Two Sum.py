876e9a349e4e78bc958b11697568b999fbdce9a2
Jundong Yi <eustaceyi@gmail.com>
2019-05-20 23:20:47 -0400

    Delete Two Sum.py

--- Two Sum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums, target):
-        dict = {}
-        for i, num in enumerate(nums):
-            if num in dict:
-                return [i, dict[num]]
-            dict[target - num] = i
-        return
