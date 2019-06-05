99af65341062d38ffff6dc5559c3378b013386e5
Jundong Yi <eustaceyi@gmail.com>
2019-06-04 22:02:46 -0400

    Create 206. Revese Linked List.py

---
+++ 206. Revese Linked List.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: 'ListNode') -> 'ListNode':
+        if not head or not head.next:
+            return head
+        node = head.next
+        prev = head
+        prev.next = None
+        while node:
+            temp = node.next
+            node.next = prev
+            prev = node
+            if temp is None:
+                break
+            node = temp
+        return node
