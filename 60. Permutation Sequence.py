1b1d340d5527c9710506679185585e3b2208a3ba
Jundong Yi <eustaceyi@gmail.com>
2019-06-24 22:03:13 -0400

    Create 60. Permutation Sequence.py

---
+++ 60. Permutation Sequence.py
@@ -0,0 +1,27 @@
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        '''
+        For permutations of n, the first (n-1)! permutations start with 1, 
+        next (n-1)! ones start with 2, ... and so on. 
+        And in each group of (n-1)! permutations, 
+        the first (n-2)! permutations start with the smallest remaining number, ...
+
+        take n = 3 as an example, the first 2 (that is, (3-1)!) permutations start with 1, 
+        next 2 start with 2 and last 2 start with 3. 
+        For the first 2 permutations (123 and 132), the 1st one (1!) starts with 2, 
+        which is the smallest remaining number (2 and 3). 
+        So we can use a loop to check the region that the sequence number falls in 
+        and get the starting digit. Then we adjust the sequence number and continue.
+        '''
+        nums = [i+1 for i in range(n)]
+        per = ''
+        k -= 1
+        while n > 0:
+            n -= 1
+            fac = 1
+            for i in range(1,n+1):
+                fac *= i
+            index, k = divmod(k, fac)
+            per += str(nums[index])
+            nums.pop(index)
+        return per
