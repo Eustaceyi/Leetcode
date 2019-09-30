1b266daa50ccd5e83f7717f2ddebafa2bdf49cd9
Jundong Yi <eustaceyi@gmail.com>
2019-09-30 18:45:06 -0400

    Update 392. Is Subsequence.py

--- 392. Is Subsequence.py
+++ 392. Is Subsequence.py
@@ -26,3 +26,16 @@ class Solution:
                     prev = temp[lo]
             
         return True
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        ps = 0
+        pt = 0
+        while ps < len(s) and pt < len(t):
+            if s[ps] == t[pt]:
+                ps += 1
+            pt += 1
+        if ps < len(s):
+            return False
+        else:
+            return True
