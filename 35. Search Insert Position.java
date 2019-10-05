b77dd7d2e84ced1abb6bfab9b6220f563ae21810
Jundong Yi <eustaceyi@gmail.com>
2019-10-05 18:53:43 -0400

    Create 35. Search Insert Position.java

---
+++ 35. Search Insert Position.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        /**
+         * Normal binary search, return lo for position
+         */
+        int lo = 0;
+        int hi = nums.length-1;
+        while (lo <= hi) {
+            int mid = (hi - lo)/2 + lo;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                lo = mid+1;
+            } else {
+                hi = mid-1;
+            }
+        }
+        return lo;
+    }
+}
