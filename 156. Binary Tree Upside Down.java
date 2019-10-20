d73b7cffc4306a7266c3147ce4089c412346672e
Jundong Yi <eustaceyi@gmail.com>
2019-10-20 17:20:20 -0400

    Update 156. Binary Tree Upside Down.java

--- 156. Binary Tree Upside Down.java
+++ 156. Binary Tree Upside Down.java
@@ -11,6 +11,8 @@ class Solution {
     /**
      * recursion method, consider that we should build from the left most and 
      * build the new tree bottom up
+     * Time O(n) since this tree is never a balanced tree
+     * Space O(n) since the stack may goes O(n) long
      * @param root
      * @return
      */
@@ -26,6 +28,35 @@ class Solution {
         root.left.right = root;
         root.left=null;
         root.right=null;
+        return newRoot;
+    }
+}
+
+class Solution {
+    /**
+     * Iterative method 
+     * Time O(n)
+     * Space O(n)
+     * @param root
+     * @return
+     */
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        Deque<TreeNode> dq = new ArrayDeque<>();
+        if (root == null || root.left == null) {
+            return root;
+        }
+        while (root != null) {
+            dq.add(root);
+            root = root.left;
+        }
+        TreeNode newRoot = dq.pollLast();
+        while (!dq.isEmpty()) {
+            TreeNode node = dq.pollLast();
+            node.left.left = node.right;
+            node.left.right = node;
+            node.left = null;
+            node.right = null;
+        }
         return newRoot;
     }
 }
