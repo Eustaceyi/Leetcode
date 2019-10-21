146b8ccf5c2b64e42e5f4a5fdf3ab40ebd7f00f5
Jundong Yi <eustaceyi@gmail.com>
2019-10-21 13:21:42 -0400

    Create 235. Lowest Common Ancestor of a Binary Search Tree.java

---
+++ 235. Lowest Common Ancestor of a Binary Search Tree.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/**
+ * recursion method
+ * Time O(h) worst condition we need to find along the longest path in the tree
+ * Space O(h) since the stack will go as deep as the height
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return root;
+        }
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+}
+
+/**
+ * iterative method
+ * Time O(h) same as above
+ * Space O(1) constant used
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode node = root;
+        while (node!=null) {
+            if (node.val > p.val && node.val > q.val) {
+                node = node.left;
+            } else if (node.val < p.val && node.val < q.val) {
+                node = node.right;
+            } else {
+                break;
+            }
+        }
+        return node;
+    }
+}
