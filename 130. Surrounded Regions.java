6fd88211ce7eda5c0773e31c3cf466226f50cd64
Jundong Yi <eustaceyi@gmail.com>
2019-11-04 16:05:54 -0500

    Update 130. Surrounded Regions.java

--- 130. Surrounded Regions.java
+++ 130. Surrounded Regions.java
@@ -41,3 +41,70 @@ class Solution {
         }
     }
 }
+/**
+ * Union find - not very suitable with this problem
+ */
+class Solution {
+    public void solve(char[][] board) {
+        if (board==null || board.length==0) {
+            return;
+        }
+        int row = board.length, col = board[0].length;
+        UF uf = new UF(row*col+1);
+        int dummy = row*col;
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                if (board[i][j] == 'O') {
+                    if (i==0 || i==row-1 || j==0 || j==col-1) {
+                        uf.union(i*col+j, dummy);
+                    } else {
+                        if (board[i-1][j] == 'O') {
+                            uf.union(i*col+j, i*col-col+j);
+                        }
+                        if (board[i][j-1] == 'O') {
+                            uf.union(i*col+j, i*col+j-1);
+                        }
+                        if (board[i+1][j] == 'O') {
+                            uf.union(i*col+j, i*col+col+j);
+                        }
+                        if (board[i][j+1] == 'O') {
+                            uf.union(i*col+j, i*col+j+1);
+                        }
+                    }
+                }
+            }
+        }
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                if (board[i][j] == 'O' && !uf.find(i*col+j, dummy)) {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+}
+
+class UF {
+    int[] ids;
+    public UF(int n) {
+        ids = new int[n];
+        for (int i=0; i<n; i++) {
+            ids[i] = i;
+        }
+    }
+    public int root(int id) {
+        while (ids[id] != id) {
+            ids[id] = ids[ids[id]];
+            id = ids[id];
+        }
+        return id;
+    }
+    public boolean find(int A, int B) {
+        return root(A) == root(B);
+    }
+    public void union(int A, int B) {
+        int x = root(A);
+        int y = root(B);
+        ids[x] = y;
+    }
+}
