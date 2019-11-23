cf2144cbc46a9fc6979639d9b937a9b4cdfb1e8c
Jundong Yi <eustaceyi@gmail.com>
2019-11-22 20:50:00 -0500

    Create 201. Bitwise AND of Numbers Range.java

---
+++ 201. Bitwise AND of Numbers Range.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        if (m == 0) {
+            return 0;
+        }
+        int count = 0;
+        while (m != n) {
+            m = m >> 1;
+            n = n >> 1;
+            count++;
+        }
+        return n << count;
+    }
+}
