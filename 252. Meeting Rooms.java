1b7c1d7ff4ba2d7151ce99d5c7fe2867a4222c06
Jundong Yi <eustaceyi@gmail.com>
2020-01-11 15:29:23 -0500

    Create 252. Meeting Rooms.java

---
+++ 252. Meeting Rooms.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        if (intervals == null || intervals.length <= 1) {
+            return true;
+        }
+        Arrays.sort(intervals, (a, b)->(a[0]-b[0]));
+        for (int i=0; i<intervals.length-1; i++) {
+            if (intervals[i][1] > intervals[i+1][0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
